4b177dc1088cac4bcd5321a537cd67f7eb096705
Ashvith Shetty <ashvithshetty10@gmail.com>
2021-11-23 22:24:31 +0530

    Replaced cc with gcc

--- phase2.sh
+++ phase2.sh
@@ -1,6 +1,6 @@
 cd Phase2/
 bison -d main.y
 flex main.l
-gcc lex.yy.c y.tab.c -ll
+gcc lex.yy.c main.tab.c -ll
 ./a.out ../valid_code.rx
-rm -rf lex.yy.c a.out y.tab.*
+rm -rf lex.yy.c a.out main.tab.*
